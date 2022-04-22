3dc08a48e2bd0ec1b4baacd547d39da7f8ba73b7
Priya7075 <102401793+Priya7075@users.noreply.github.com>
2022-04-22 16:33:53 +0530

    Add files via upload

---
+++ add-fun-prog.py
@@ -0,0 +1,12 @@
+#Addition of two number using functions
+from calendar import c
+
+
+# def s(a,b):
+#     c=a+b
+#     d=a+b
+#     print('sum is ',c)  #sum is 30
+#     print('sum is ',d)#sum is 12.8
+
+# s(10,20) 
+# s(10.5,2.3)   
---
+++ default-arg-prog.py
@@ -0,0 +1,17 @@
+# def info(name="HEY", age=22):   
+#     print('Name :', name)       
+#     print('Age :', age)
+# info()      #Name:HEY
+#             #Age:22
+# info('VI')
+#   # Name:VI  
+#   # Age:22           
+# info('NB', 22)
+# #Name:NB
+# #Age:22
+# info(name='nani')
+# #Name:nani
+# #Age:22
+# info('nainu')
+# #Name:nainu
+# #name:22
